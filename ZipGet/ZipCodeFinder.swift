ee7a1950e0a4448c2c9b257a1a0d3c54335ec8fe
Andrew <jandrewmoore@me.com>
2014-08-07 11:05:30 -0400

    Switch from AFNetworking to Alamofire. Add and use SwiftyJSON.

--- ZipGet/ZipCodeFinder.swift
+++ ZipGet/ZipCodeFinder.swift
@@ -21,35 +21,33 @@ class ZipCodeFinder {
     }
         
     func findZipCode(forCoordinate coord: CLLocationCoordinate2D, onSuccess: (String) -> Void, onError: (String?) -> Void) {
-        let url = "http://api.geonames.org/findNearbyPostalCodesJSON" +
-            "?lat=\(coord.latitude)&lng=\(coord.longitude)&username=\(username)&maxRows=1"
+        let url = "http://api.geonames.org/findNearbyPostalCodesJSON"
+        let parameters: [String: AnyObject] = [
+            "lat": coord.latitude,
+            "lng": coord.longitude,
+            "username": username,
+            "maxRows": 1
+        ]
         latestCoordinates = coord
 
-        manager.GET(url,
-            parameters: nil,
-            success: { (op: AFHTTPRequestOperation!, response: AnyObject!) in
-                let json = response as [String:AnyObject]
-                let postalCodes: AnyObject? = json["postalCodes"]
-                let postalCodesArr = postalCodes as [[String:AnyObject]]
-                if !postalCodesArr.isEmpty {
-                    let first = postalCodesArr[0]
-                    if let res: AnyObject = first["postalCode"] {
-                        onSuccess(res as String)
-                        return
-                    }
-                }
-                
+        Alamofire.request(.GET, url, parameters: parameters).response { _, _, data, _ in
+            let json = JSONValue(data as NSData)
+
+            if let message = json["status"]["message"].string {
+                onError("Error: \(message)")
+            } else if let postalCode = json["postalCodes"][0]["postalCode"].string {
+                onSuccess(postalCode as String)
+            } else {
                 NSLog("No postal code at \(coord.latitude), \(coord.longitude)")
                 onError("Couldn't find a postal code here 😱")
-            }, failure: { (op: AFHTTPRequestOperation!, error: NSError!) in
-                onError(error.localizedDescription)
-            })
+            }
+        }
     }
     
     func findZipCode(forCityName city: String, onSuccess: (String) -> Void, onError: (String?) -> Void) {
         var url = "http://api.geonames.org/searchJSON"
         
-        var parameters: [String:AnyObject] = [
+        var parameters: [String: AnyObject] = [
             "username": username,
             "maxRows": 1
         ]
@@ -61,32 +59,22 @@ class ZipCodeFinder {
         if cityParts.count > 1 {
             parameters["adminCode1"] = cityParts[1]
         }
-        
-        manager.GET(url,
-            parameters: parameters,
-            success: { (op: AFHTTPRequestOperation!, response: AnyObject!) in
-                let json = response as [String:AnyObject]
-                let results: AnyObject? = json["geonames"]
-                let resultsArray = results as [[String:AnyObject]]
-                if !resultsArray.isEmpty {
-                    let first = resultsArray[0]
-                    var lat: AnyObject? = first["lat"]
-                    var lng: AnyObject? = first["lng"]
 
-                    lat = lat as? String
-                    lng = lng as? String
-                    
-                    if lat && lng {
-                        self.latestCoordinates = CLLocationCoordinate2DMake(lat!.doubleValue, lng!.doubleValue)
-                        self.findZipCode(forCoordinate: self.latestCoordinates!, onSuccess: onSuccess, onError: onError)
-                        return
-                    }
-                }
-                
-                onError("Could not find '\(city)'")
-            },
-            failure: { (op: AFHTTPRequestOperation!, error: NSError!) in
-                onError(error.localizedDescription)
-            })
+        Alamofire.request(.GET, url, parameters: parameters).response { _, _, data, _ in
+            let json = JSONValue(data as NSData)
+
+            if let message = json["status"]["message"].string {
+                onError("Error: \(message)")
+            }
+
+            let lat = json["geonames"][0]["lat"].double
+            let lng = json["geonames"][0]["lng"].double
+            if lat && lng {
+                self.latestCoordinates = CLLocationCoordinate2DMake(lat!, lng!)
+                self.findZipCode(forCoordinate: self.latestCoordinates!, onSuccess: onSuccess, onError: onError)
+            } else {
+                onError("Is '\(city)' even a real place?")
+            }
+        }
     }
 }
